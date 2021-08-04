e7e2b40f89a963b04dae4e98cc462f07d21eb550
gcca <cristhian@personal.com>
2021-08-04 15:34:00 -0500

    [master] add import tag

---
+++ upy/templatetags/upy.py
@@ -0,0 +1,31 @@
+from django.template import Library, Node
+from django.template.base import FilterExpression, Parser, Token
+from django.template.loader_tags import construct_relative_path
+
+
+register = Library()
+
+
+class UpyImportNode(Node):
+
+  def __init__(self, template: FilterExpression, *args, **kwargs):
+    self.template = template
+    super().__init__(*args, **kwargs)
+
+  def render(self, context):
+    # TODO: improve using cache
+    subpath = self.template.resolve(context)
+    fullpath = (construct_relative_path(self.origin.template_name, subpath),)
+    template = context.template.engine.select_template(fullpath)
+    return template.render(context)
+
+
+@register.tag
+def upy_import(parser: Parser, token: Token):
+  bits = token.split_contents()
+  if len(bits) < 2:
+    raise template.TemplateSyntaxError(
+      f'{bits[0]} tag takes at least one argument: the asset path')
+  bits[1] = construct_relative_path(
+    parser.origin.template_name, bits[1])
+  return UpyImportNode(parser.compile_filter(bits[1]))
