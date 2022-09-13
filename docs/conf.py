ba18ec3253270eda650a8d88d552380a2b90f50b
Cristhian <143498+gcca@users.noreply.github.com>
2022-09-13 14:04:16 -0500

    [docs] sphinx docs (#7)
    Co-authored-by: gcca <cristhian.gonzales@neomdas.com>

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'neompy'
+copyright = '2022'
+author = 'neomadas'
+release = '0.0.4'
+
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
